7727afd61bdb4a238302fe2f909dfabe50ea5426
Alessandro Gerelli <geroale2000@gmail.com>
2023-07-12 12:03:32 +0200

    Update Lyricsify.ts

--- src/providers/Lyricsify.ts
+++ src/providers/Lyricsify.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { Provider, SearchParams } from './Provider'
 import { parse } from 'node-html-parser'
 import { decode } from 'html-entities'
@@ -6,12 +6,12 @@ import { normalizeString, removeTags } from '../utils'
 
 const BASE_URL = 'https://www.lyricsify.com/'
 
-const config = {
+const config: AxiosRequestConfig = {
   headers: {
     'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
     // 'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Encoding': 'text/html; charset=UTF-8',
     // 'Content-Encoding': 'br',
     'Content-Type': 'text/html; charset=utf-8',
     'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8,hr;q=0.7',
@@ -19,13 +19,15 @@ const config = {
       'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     Referer: 'https://www.lyricsify.com/',
   },
+  withCredentials: true,
+  responseType: 'text'
 }
 
 export class Lyricsify implements Provider {
   private async getLink(artist: string, name: string) {
     const normalizedArtist = normalizeString(artist)
     const normalizedName = normalizeString(name)
-    const query = artist + ' ' + name
+    const query = encodeURIComponent(artist + ' ' + name)
     const response = await axios.get(BASE_URL + 'search?q=' + query, config)
     const data = response.data
     const list = parse(data)
